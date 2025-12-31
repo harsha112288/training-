56c5c7365c22d38c386300e5da2ac45aea830334
harsha112288 <pavanharsha605@gmail.com>
2025-12-31 10:31:50 +0530

    ArithmeticOperators

---
+++ ArithmeticOperators.java
@@ -0,0 +1,8 @@
+public class ArithmeticOperators {
+    public static void main(String []args){
+        int result=10+5*2;
+        System.out.println(result);
+        int result2=(10+5)*2;
+        System.out.println(result2);
+    }
+}
